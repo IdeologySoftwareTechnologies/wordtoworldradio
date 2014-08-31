882ae4fc68b908ee55b83266e0b927edf5ed16a2
iashanmugavel <iashanmugavel@gmail.com>
2014-08-31 22:27:56 +0530

    changed layout file and added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,18 @@
-# == Route Map
-#
-#                    Prefix Verb   URI Pattern                              Controller#Action
-#         new_admin_session GET    /admin/login(.:format)                   devise/sessions#new
-#             admin_session POST   /admin/login(.:format)                   devise/sessions#create
-#     destroy_admin_session DELETE /admin/logout(.:format)                  devise/sessions#destroy
-#            admin_password POST   /admin/secret(.:format)                  devise/passwords#create
-#        new_admin_password GET    /admin/secret/new(.:format)              devise/passwords#new
-#       edit_admin_password GET    /admin/secret/edit(.:format)             devise/passwords#edit
-#                           PATCH  /admin/secret(.:format)                  devise/passwords#update
-#                           PUT    /admin/secret(.:format)                  devise/passwords#update
-# cancel_admin_registration GET    /admin/register/cancel(.:format)         devise/registrations#cancel
-#        admin_registration POST   /admin/register(.:format)                devise/registrations#create
-#    new_admin_registration GET    /admin/register/cmon_let_me_in(.:format) devise/registrations#new
-#   edit_admin_registration GET    /admin/register/edit(.:format)           devise/registrations#edit
-#                           PATCH  /admin/register(.:format)                devise/registrations#update
-#                           PUT    /admin/register(.:format)                devise/registrations#update
-#                           DELETE /admin/register(.:format)                devise/registrations#destroy
-#        admin_confirmation POST   /admin/verification(.:format)            devise/confirmations#create
-#    new_admin_confirmation GET    /admin/verification/new(.:format)        devise/confirmations#new
-#                           GET    /admin/verification(.:format)            devise/confirmations#show
-#              admin_unlock POST   /admin/unblock(.:format)                 devise/unlocks#create
-#          new_admin_unlock GET    /admin/unblock/new(.:format)             devise/unlocks#new
-#                           GET    /admin/unblock(.:format)                 devise/unlocks#show
-#
 
 Rails.application.routes.draw do
-  
+    root 'welcome#index'
   get 'welcome/index'
 
   #devise_for :admins
+  
   devise_for :admins, path: "admin", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
